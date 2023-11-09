71953b19bcf3c028e027824d0f5870d475140369
Thoffy <126025184+Thoffyy@users.noreply.github.com>
2023-11-09 16:24:17 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {Container, Row} from 'react-bootstrap';
+import {Container, Row, Image} from 'react-bootstrap';
 
 ReactDOM.render(
 
-  <Container>
-
-<Row>
-  <h1>ola mundo</h1>
+  <Container className="banner" fluid>
+<Row className="row1">
+  <h1 className="white-text">ola mundo</h1>
 </Row>  
 
+<Row className="row2">
+  <Image roundedCircle src="rick.gif" />
+</Row>
   </Container>
 ,
   document.getElementById('root')
