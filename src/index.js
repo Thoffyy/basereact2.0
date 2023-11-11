ab01f0d3309155bede8c15d67d2b52d58e53dd84
Thoffy <bigbigthoffy@proton.me>
2023-11-11 18:23:45 -0300

    adicionado texto abaixo do banner
    aaa

--- src/index.js
+++ src/index.js
@@ -3,18 +3,77 @@ import * as ReactDOM from 'react-dom'
 import {Container, Row, Image} from 'react-bootstrap';
 
 ReactDOM.render(
-
-  <Container fluid className="banner" >
-<Row className="row1">
+<>
+    <Container fluid className="banner" >
+  <Row className="row1">
   <h1 className="white-text">ola mundo</h1>
-</Row>  
+  </Row>  
 
-<Row className="row2">
-  
+  <Row className="row2">
+
   <Image roundedCircle src="defaultAvatar.png" />
 
-</Row>
+  </Row>
+    </Container>
+  <Container>
+
+<h2>
+  Quem sou eu?
+</h2>
+
+<p>
+A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo rea
+</p>
+
+<h2>
+  coisas aleatorias
+</h2>
+
+<p>
+A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo rea
+</p>
+
+<h2>
+  Lorem Ipsus
+</h2>
+
+<p>
+A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo rea
+</p>
+
+<h2>
+  Asid amet
+</h2>
+
+<p>
+A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo rea
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   </Container>
+
+
+
+
+
+
+
+
+</>
+
 ,
   document.getElementById('root')
 );
